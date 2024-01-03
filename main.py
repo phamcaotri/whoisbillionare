264bd50a046f614fe65631701c2744460a2bd9b4
phamcaotri <22521525@gm.uit.edu.vn>
2024-01-03 02:52:33 +0000

    Jan 3, 2024, 9:52 AM

--- main.py
+++
@@ -1,9 +0,0 @@
-from typing import *
-
-class Estate:
-    def __init__(self, name: str, price: int):
-        self.name = name
-        self.price = price
-
-    def __repr__(self):
-        return f"{self.name} {self.price}"
